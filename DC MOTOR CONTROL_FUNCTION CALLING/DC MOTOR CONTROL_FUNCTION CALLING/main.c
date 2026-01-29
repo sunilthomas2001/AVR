c1550f5c3f57a8b9f10461023422720e80c6070d
Sunil Thomas <sunilcthomas008@gmail.com>
2026-01-29 18:21:18 +0530

    added avr codes

---
+++ DC MOTOR CONTROL_FUNCTION CALLING/DC MOTOR CONTROL_FUNCTION CALLING/main.c
@@ -0,0 +1,36 @@
+/*
+ * DC MOTOR CONTROL_FUNCTION CALLING.c
+ *
+ * Created: 29-01-2026 18:03:59
+ * Author : Sunil Thomas
+ */
+
+
+
+
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB |= (1 << PB0) | (1 << PB1);  
+	while (1)
+	{
+		PORTB |= (1 << PB0);
+		PORTB &= ~(1 << PB1);
+		_delay_ms(2000);
+		
+		PORTB &= ~(1 << PB0);
+		PORTB |= (1 << PB1);
+		_delay_ms(1000);
+		
+		PORTB &= ~(1 << PB0);
+		PORTB &= ~(1 << PB1);
+		_delay_ms(2000);
+	}
+}
+
+
